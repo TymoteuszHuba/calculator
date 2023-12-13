9f85ae9302b0cfc622241279112286ffd28cc89a
Tymoteusz Huba <tymoteuszhuba@MacBook-Air-Tymoteusz.local>
2023-12-13 13:00:21 +0100

    ver2 of calculator. Updated/changed all functions with switch solution of calculate values. It is some problems with calculations of division and minus numbers

--- main.js
+++ main.js
@@ -3,77 +3,106 @@ const number = document.querySelectorAll('.number');
 const ac = document.querySelector('.ac');
 const del = document.querySelector('.del');
 const btns = document.querySelectorAll('button');
-let numberArray = [];
-let count = 0;
-let chainString = '';
+let firstVal = '0';
+let firstFloatNumber;
+let secondFloatNumber;
+let operator = '';
+let displayFirstVal = output.lastElementChild;
+let displaySecVal = output.firstElementChild;
 
-// function which add comma and set integer numbers
-const commaSet = (value, currValue) => {
-	chainString += value;
-	let integerNumbers = parseFloat(chainString);
-	let numbers = new Intl.NumberFormat('en-US').format(integerNumbers);
-	currValue.textContent = numbers;
+output.lastElementChild.textContent = firstVal;
+
+const operate = () => {
+	let result;
+	switch (operator) {
+		case '+':
+			result = firstFloatNumber + secondFloatNumber;
+			break;
+		case '-':
+			result = firstFloatNumber - secondFloatNumber;
+			break;
+		case '*':
+			result = firstFloatNumber * secondFloatNumber;
+			break;
+		case '/':
+			result = firstFloatNumber / secondFloatNumber;
+			break;
+		case '%':
+			result = firstFloatNumber % secondFloatNumber;
+			break;
+		default:
+			result = firstFloatNumber;
+			break;
+	}
+
+	displaySecVal.textContent = result.toLocaleString('en-us', {
+		maximumFractionDigits: 8,
+	});
+
+	displayFirstVal.textContent = '';
+	firstFloatNumber = result;
+	firstVal = result.toString();
 };
 
-const putNumbers = (val) => {
-	numberArray.push(chainString);
-	console.log(numberArray);
-	output.firstElementChild.textContent =
-		val + ' ' + output.lastElementChild.textContent;
-	output.lastElementChild.textContent = '';
-	chainString = '';
+const setFirstVal = (val) => {
+	firstVal += val;
+	firstFloatNumber = parseFloat(firstVal);
+	let showFirstVal = firstFloatNumber.toLocaleString('en-US', {
+		maximumFractionDigits: 8,
+	});
+	displayFirstVal.textContent = showFirstVal;
+	console.log('firstFloatNumber: ', firstFloatNumber);
 };
 
-const sum = () => {
-	putNumbers();
-	let a = parseFloat(numberArray[0]);
-	let b = parseFloat(numberArray[1]);
+const setSecondVal = (op) => {
+	displaySecVal.textContent = op + ' ' + displayFirstVal.textContent;
+	let secVal = firstVal;
+	secondFloatNumber = parseFloat(secVal);
+	displayFirstVal.textContent = '';
+	firstVal = '';
+	firstFloatNumber = 0;
+};
 
-	let sum = a + b;
-	console.log(sum);
+const displayCalc = (value) => {
+	if ((value > -1 && value < 10) || value === '.') {
+		setFirstVal(value);
+	} else if (
+		value === 'x' ||
+		value === '*' ||
+		value === '/' ||
+		value === '+' ||
+		value === '-' ||
+		value === '%'
+	) {
+		setSecondVal(value);
+		operator = value === 'x' ? '*' : value;
+		console.log('firstFloatNumber: ', firstFloatNumber);
+		console.log('secondFloatNumber: ', secondFloatNumber);
+	} else if (value === '=') {
+		operate();
+	} else if (value === 'AC' || value === 'c' || value === 'C') {
+		clearCalc();
+	}
 };
 
 // function which is responsible for clear calculator
 const clearCalc = () => {
 	output.firstElementChild.textContent = '';
-	output.lastElementChild.textContent = '';
-	count = 0;
-	chainString = '';
-	numberArray = [];
-};
-
-// main function which update calculator for each click from keyboard or icon from calculator. fires correct functions if clicked button was correct
-const updateCalc = (value) => {
-	const calcValue = output.lastElementChild;
-	// const currValue = lastElementCalc.textContent;
-
-	if (value > -1 && value < 10) {
-		// lastElementCalc.textContent += value;
-		count++;
-
-		commaSet(value, calcValue);
-	} else if (value === 'Backspace') {
-		// console.log(chainString);
-		calcValue.textContent = calcValue.textContent.slice(0, -1);
-	} else if (value === '+') {
-		putNumbers(value);
-	} else if (value === '=' || value === 'Enter') {
-		console.log(value);
-	} else if (value === 'c' || value === 'C' || value === 'AC') {
-		clearCalc();
-	}
+	output.lastElementChild.textContent = '0';
+	firstVal = '';
 };
 
 btns.forEach((button) => {
 	button.addEventListener('click', () => {
-		console.log(button.textContent);
+		// console.log(button.textContent);
 		const numberBtn = button.textContent;
-		updateCalc(numberBtn);
+		displayCalc(numberBtn);
 	});
 });
 
 window.addEventListener('keydown', (event) => {
 	const pressedKey = event.key;
-	updateCalc(pressedKey);
+	displayCalc(pressedKey);
 });
-ac.addEventListener('click', clearCalc);
+
+// ac.addEventListener('click', clearCalc);
